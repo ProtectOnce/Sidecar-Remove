c8d455a5861cb76e07dd6ed7cde14b1020d733be
Saurabh Sharma <86997945+sharmajic@users.noreply.github.com>
2023-09-22 18:43:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+const { ECSClient, ListTasksCommand, DescribeTasksCommand, DescribeTaskDefinitionCommand, RegisterTaskDefinitionCommand, UpdateServiceCommand } = require("@aws-sdk/client-ecs");
+
+const CLUSTER_NAME = 'my-ecs-cluster-rcnaggce';
+const REGION = 'us-east-1';
+
+const ecs = new ECSClient({ region: REGION }); 
+
+
+async function listTasks(clusterName) {
+  const tasks = [];
+  let nextToken;
+
+  do {
+    const params = {
+      cluster: clusterName,
+      nextToken,
+    };
+
+    const command = new ListTasksCommand(params);
+    const response = await ecs.send(command);
+
+    tasks.push(...response.taskArns);
+    nextToken = response.nextToken;
+  } while (nextToken);
+
+  return tasks;
+}
+
+
+async function removeJokerSidecar(taskDefinitionArn) {
+  const params = {
+    taskDefinition: taskDefinitionArn,
+  };
+
+  const command = new DescribeTaskDefinitionCommand(params);
+  const response = await ecs.send(command);
+
+  const taskDefinition = response.taskDefinition;
+
+  // Check if the task definition has a "joker" sidecar
+  taskDefinition.containerDefinitions = taskDefinition.containerDefinitions.filter(
+    (container) => container.name !== 'side-car-ProtectOnce-Agentless-Monitor-script'
+  );
+
+  // Register the updated task definition
+  const registerParams = {
+    ...taskDefinition,
+  };
+
+  const registerCommand = new RegisterTaskDefinitionCommand(registerParams);
+  const registerResponse = await ecs.send(registerCommand);
+
+  return registerResponse.taskDefinition.taskDefinitionArn;
+}
+
+
+async function main() {
+  
+  const clusterName = CLUSTER_NAME;
+
+  if (!clusterName) {
+    console.error('Usage: node script.js <ECS cluster name>');
+    return;
+  }
+
+  
+  const tasks = await listTasks(clusterName);
+
+  
+  for (const taskArn of tasks) {
+    // Describe the task to get the task definition ARN
+    const taskParams = {
+      cluster: clusterName,
+      tasks: [taskArn],
+    };
+
+    const taskCommand = new DescribeTasksCommand(taskParams);
+    const taskResponse = await ecs.send(taskCommand);
+    const task = taskResponse.tasks[0];
+    const taskDefinitionArn = task.taskDefinitionArn;
+    console.log("taskARn:",taskDefinitionArn);
+    // Remove "joker" sidecar from the task definition
+    const newTaskDefinitionArn = await removeJokerSidecar(taskDefinitionArn);
+
+    // Update the task with the new task definition
+    const updateServiceParams = {
+      cluster: clusterName,
+      service: task.group.split(":")[1],
+      taskDefinition: newTaskDefinitionArn,
+    };
+    console.log(JSON.stringify(task));
+    const updateServiceCommand = new UpdateServiceCommand(updateServiceParams);
+    await ecs.send(updateServiceCommand);
+
+    console.log(`Removed 'joker' sidecar from task: ${taskArn}`);
+  }
+}
+
+main();
